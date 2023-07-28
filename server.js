e10bc6cb1f5beef227cf44e0f7320c4c4190b00e
Essence1987 <129245370+Essence1987@users.noreply.github.com>
2023-07-27 21:05:17 -0600

    added authRoutes to server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const uuid = require('uuid');
+const authRoutes = require('./controllers/api/auth.js')
 // initalize sequelize with session store
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
@@ -33,6 +34,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
+app.use(authRoutes);
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
