f51bc457097d97beffad46ebbe01048fb10b1da4
Justin <justin.martin@hsc.utah.edu>
2023-07-29 14:42:58 -0600

    added to the student.js create student table

--- models/students.js
+++ models/students.js
@@ -1,21 +1,43 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes, Model } = require('sequelize');
 const sequelize = require('../config/database');
 
-const Student = sequelize.define('Student', { 
-  StudentID: {
-    type: DataTypes.UUID,
-    defaultValue: DataTypes.UUIDV4
-    primaryKey: true,
-    autoIncrement: false,
-  },
-  StudentName: {
-    type: DataTypes.STRING(100),
-    allowNull: false,
-  },
-  YearOfTraining: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-  },
-});
+class Students extends Model {}
 
-module.exports = Student;
+Students.init(
+  {
+    StudentID: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+      autoIncrement: false,
+    },
+    StudentName: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    YearOfTraining: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  },
+);   
+
+
+async function createStudentTable() {
+  try {
+    await Students.sync({ force: true });
+    console.log('Student table was created successfully.');
+  } catch (error) {
+    console.log('Error creating Student table:', error);
+  }
+}
+
+{
+  sequelize,
+  timestamps: false,
+  freezeTableName: true,
+  underscored: true,
+  modelName: 'students',
+}
+
+module.exports = Students;
