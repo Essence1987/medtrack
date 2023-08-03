fff811d3e18faad49de3cfa1392631018d112158
camilleramos1 <129894673+camilleramos1@users.noreply.github.com>
2023-08-02 18:30:34 -0600

    updated server.js and updated user model

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
 
 // const apiRoutes = require('./api');
-// const dashboardRoutes = require('./dashboard');
+const dashboardRoutes = require('./dashboard');
 const homeRoutes = require('./homepage');
 
 // router.use('/api', apiRoutes);
-// router.use('/dashboard', dashboardRoutes);
+router.use('/dashboard', dashboardRoutes);
 router.use('/', homeRoutes);
 
 module.exports = router;
--- seeds/index.js
+++ seeds/index.js
@@ -1,31 +1,17 @@
+const sequelize = require('../config/connection');
+const { User } = require('../models');
+const userData = require('./userSeedData.json');
+
 const seedUser = () => User.bulkCreate(userData,{
     individualHooks:true,
     returning:true
-  });
-
-const seedComments = require('./comments-seeds');
-const seedPost = require('./post-seeds');
-const seedUser = require('./user-seeds');
-
-
-const sequelize = require('../config/connection');
+});
 
 const seedAll = async () => {
   await sequelize.sync({ force: true });
   console.log('\n----- DATABASE SYNCED -----\n');
-   await seedUser();
-  console.log('\n----- USER SEEDED -----\n');
-
-  await seedPost();
-  console.log('\n----- POST SEEDED -----\n');
-
-  await seedComments();
-  console.log('\n----- COMMENTS SEEDED -----\n');
-
- 
-
-
-
+    await seedUser();
+    
   process.exit(0);
 };
 
