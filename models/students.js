c4e301c481d239f29f5a6378ffe7d91c8573e2f7
Justin <justin.martin@hsc.utah.edu>
2023-08-05 11:51:19 -0600

    homepage language

---
+++ models/students.js
@@ -0,0 +1,29 @@
+// const { DataTypes, Model } = require("sequelize");
+// const sequelize = require("../config/database");
+
+// class Students extends Model {}
+
+// Students.init(
+//   {
+//     StudentID: {
+//       type: DataTypes.UUID,
+//       defaultValue: DataTypes.UUIDV4,
+//       primaryKey: true,
+//       autoIncrement: false,
+//     },
+//     StudentName: {
+//       type: DataTypes.STRING(100),
+//       allowNull: false,
+//     },
+  
+//   },
+//   {
+//     sequelize,
+//     timestamps: false,
+//     freezeTableName: true,
+//     underscored: true,
+//     modelName: "students",
+//   }
+// );
+
+// module.exports = Students;
